aa7e2fe1fbc845681cf4f09b4742a8c62be400c9
Otto <ottolaitinen@outlook.com>
2022-04-08 12:13:41 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-import React from 'react'
-import CarForm from './Form'
-=======
 import {Thomas} from './components/Thomas'
->>>>>>> 5378136a1bd5676db3fe77c35643e004e6f32ec5
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <CarForm/>
-=======
       <Thomas/>
       
 
->>>>>>> 5378136a1bd5676db3fe77c35643e004e6f32ec5
     </div>
   );
 }
