d9a79d48068fb5a794e024423d8904b7c33a3577
Thomás <thomas.rizziomura@edu.turkuamk.fi>
2022-04-15 10:56:49 +0300

    Averages added

--- src/components/TotalStats.js
+++ src/components/TotalStats.js
@@ -5,17 +5,25 @@ export const TotalStats = () => {
   const context = useContext(GlobalContext);
 
   // List of all the expenses
-  const prices = context.fuelExpenses.map(fuelExpense => parseInt(fuelExpense.priceRefuel));
+  const prices1 = context.fuelExpenses.map(fuelExpense => parseInt(fuelExpense.distance));
+  const prices2 = context.fuelExpenses.map(fuelExpense => parseInt(fuelExpense.litersRefuel));
+  const prices3 = context.fuelExpenses.map(fuelExpense => parseInt(fuelExpense.priceRefuel));
 
   // Average
-  const priceAverage = prices.reduce((acc, item) => (acc += item), 0).toFixed(2) / prices.length
+  const priceAverage1 = prices1.reduce((acc, item) => (acc += item), 0).toFixed(2) / prices1.length
+  const priceAverage2 = prices2.reduce((acc, item) => (acc += item), 0).toFixed(2) / prices2.length
+  const priceAverage3 = prices3.reduce((acc, item) => (acc += item), 0).toFixed(2) / prices3.length
 
   return (
     <div>
-
+             <h2> Average prices:</h2>
         <div>
-            <h4>Average price</h4>
-            <p className="">${priceAverage}</p>
+            <h4>Distance</h4>
+            <p className="">{priceAverage1}</p>
+            <h4>Liters refuel</h4>
+            <p className="">{priceAverage2}</p>
+            <h4>Price refuel</h4>
+            <p className="">${priceAverage3}</p>
         </div>
 
     </div>
