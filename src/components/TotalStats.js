54d0a6327ad37c7927613afa0f9140e42738de40
saa1saar <salli.saarinen@edu.turkuamk.fi>
2022-04-15 11:10:27 +0300

    totals added

--- src/components/TotalStats.js
+++ src/components/TotalStats.js
@@ -8,24 +8,40 @@ export const TotalStats = () => {
   const prices1 = context.fuelExpenses.map(fuelExpense => parseInt(fuelExpense.distance));
   const prices2 = context.fuelExpenses.map(fuelExpense => parseInt(fuelExpense.litersRefuel));
   const prices3 = context.fuelExpenses.map(fuelExpense => parseInt(fuelExpense.priceRefuel));
-
-  // Average
+  const names = context.fuelExpenses.map(fuelExpense => (fuelExpense.name));
+  
+  //Totals
+    const namesTotal = names.length;
+    const distanceTotal = prices1.reduce((acc,item)=> acc + item,0);
+    const litersTotal = prices2.reduce((acc,item)=> acc + item,0);
+    const priceTotal = prices3.reduce((acc,item)=> acc + item,0);
+  
+    // Average
   const priceAverage1 = prices1.reduce((acc, item) => (acc += item), 0).toFixed(2) / prices1.length
   const priceAverage2 = prices2.reduce((acc, item) => (acc += item), 0).toFixed(2) / prices2.length
   const priceAverage3 = prices3.reduce((acc, item) => (acc += item), 0).toFixed(2) / prices3.length
 
   return (
     <div>
-             <h2> Average prices:</h2>
-        <div>
+            <h2>Totals</h2>
+            <h4>Cars</h4>
+            <p classname="">{namesTotal}</p>
+            <h4>Distance</h4>
+            <p classname="">{distanceTotal}</p>
+            <h4>Liters</h4>
+            <p classname="">{litersTotal}</p>
+            <h4>Price</h4>
+            <p classname="">${priceTotal}</p>
+
+            <h2>Average prices:</h2>
             <h4>Distance</h4>
             <p className="">{priceAverage1}</p>
             <h4>Liters refuel</h4>
             <p className="">{priceAverage2}</p>
             <h4>Price refuel</h4>
             <p className="">${priceAverage3}</p>
-        </div>
+        
 
     </div>
   )
-}
+} 
