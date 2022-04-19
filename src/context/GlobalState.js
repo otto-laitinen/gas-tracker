940c4842d6c60da75d267d091be3f4e3dde7a7d0
Otto <ottolaitinen@outlook.com>
2022-04-19 09:14:51 +0300

    more cleanup

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,17 +1,13 @@
 import React, {createContext, useReducer } from "react";
 import AppReducer from './AppReducer'
 
-// Initial state
 const initialState = {fuelExpenses: []}
 
-// Create context
 export const GlobalContext = createContext(initialState);
 
-// Provider component
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    // Add fuel expense
     function addFuelExpense(fuelExpense){
         dispatch({
             type: 'ADD_FUEL_EXPENSE',
